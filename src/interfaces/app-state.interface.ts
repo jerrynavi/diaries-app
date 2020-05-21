5ea334c6303cfa9bb3c94af22b06bb85c5f79375
Jerry Ejonavi <jerryasih@gmail.com>
2020-05-21 17:58:06 +0100

    normalize state

--- src/interfaces/app-state.interface.ts
+++ src/interfaces/app-state.interface.ts
@@ -1,5 +1,6 @@
 import { User } from './user.interface';
 import { Diary } from './diary.interface';
+import { DiaryEntry } from './diary-entry.interface';
 
 export interface AppState {
   auth: {
@@ -8,6 +9,7 @@ export interface AppState {
   };
   user: User | null;
   diaries: Diary[] | null;
+  diaryEntries: DiaryEntry[];
   app: {
     loading: boolean;
     currentLocation: string;
--- src/interfaces/diary.interface.ts
+++ src/interfaces/diary.interface.ts
@@ -1,9 +1,6 @@
-import { DiaryEntry } from './diary-entry.interface';
-
 export interface Diary {
   id: number;
   title: string;
-  entries: DiaryEntry[];
   type: 'private' | 'public';
   createdAt: string;
   updatedAt: string;
