79d84734f7bc7dc55d48d7d348cf4e41a32dee21
Jerry Ejonavi <jerryasih@gmail.com>
2020-05-23 07:39:07 +0100

    add user slice

--- src/app/rootReducer.ts
+++ src/app/rootReducer.ts
@@ -1,14 +1,16 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import appReducer from './appSlice';
 import authReducer from '../features/auth/authSlice';
+import userReducer from '../features/auth/userSlice';
 import diariesReducer from '../features/diary/diariesSlice';
-import entriesSlice from '../features/entry/entriesSlice';
+import entriesReducer from '../features/entry/entriesSlice';
 
 const rootReducer = combineReducers({
   app: appReducer,
   auth: authReducer,
   diaries: diariesReducer,
-  entries: entriesSlice,
+  entries: entriesReducer,
+  user: userReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
---
+++ src/features/auth/userSlice.ts
@@ -0,0 +1,21 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { User } from '../../interfaces/user.interface';
+
+const user = createSlice({
+  name: 'user',
+  initialState: null as User | null,
+  reducers: {
+    setUser(state, { payload }: PayloadAction<User>) {
+      state = payload;
+    },
+    clearUser(state) {
+      if (state) {
+        return (state = null);
+      }
+    },
+  },
+});
+
+export const { setUser, clearUser } = user.actions;
+
+export default user.reducer;
