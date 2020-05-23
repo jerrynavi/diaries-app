8d62da133613e0c8c695c6d787f8e1902eb573e5
Jerry Ejonavi <jerryasih@gmail.com>
2020-05-23 01:33:31 +0100

    setup axios error handler

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,21 +1,37 @@
-import axios, { AxiosInstance, AxiosResponse } from 'axios';
+import axios, { AxiosInstance, AxiosResponse, AxiosError } from 'axios';
 
-const httpClient: AxiosInstance = axios.create({
+const http: AxiosInstance = axios.create({
   baseURL: 'https://diaries.app',
 });
 
-httpClient.defaults.headers.post['Content-Type'] = 'application/json';
+http.defaults.headers.post['Content-Type'] = 'application/json';
 
-httpClient.interceptors.response.use(
+http.interceptors.response.use(
   async (response: AxiosResponse): Promise<any> => {
     if (response.status >= 200 && response.status < 300) {
       return response.data;
     }
   },
-  (error) => {
-    console.log('Error', error.message);
-    return Promise.reject(error);
+  (error: AxiosError) => {
+    const {
+      response,
+      request,
+    }: { response?: AxiosResponse; request?: XMLHttpRequest } = error;
+    if (response) {
+      if (response.status >= 400 && response.status < 500) {
+        return {
+          message: response.data?.message ?? 'An error occurred',
+          isError: true,
+        };
+      }
+    } else if (request) {
+      return {
+        message: 'Request failed. Please try again.',
+        isError: true,
+      };
+    }
+    return Promise.reject(error.response);
   }
 );
 
-export default httpClient;
+export default http;
