86559f218fd3b1c1d4014e6b47c41234d5a3367b
Jerry Ejonavi <jerryasih@gmail.com>
2020-05-25 02:03:28 +0100

    use sweetalert for notifications

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosInstance, AxiosResponse, AxiosError } from 'axios';
+import Swal from 'sweetalert2';
 
 const http: AxiosInstance = axios.create({
   baseURL: 'https://diaries.app',
@@ -19,10 +20,25 @@ http.interceptors.response.use(
     }: { response?: AxiosResponse; request?: XMLHttpRequest } = error;
     if (response) {
       if (response.status >= 400 && response.status < 500) {
-        return {
-          message: response.data?.data?.message ?? 'An error occurred',
-          isError: true,
-        };
+        return Swal.fire({
+          titleText: response.data?.data?.message ?? 'An error occurred',
+          position: 'top-end',
+          timer: 3000,
+          timerProgressBar: true,
+          toast: true,
+          showConfirmButton: false,
+          showCancelButton: true,
+          cancelButtonText: 'Dismiss',
+          icon: 'error',
+          showClass: {
+            popup: 'swal2-noanimation',
+            backdrop: 'swal2-noanimation',
+          },
+          hideClass: {
+            popup: '',
+            backdrop: '',
+          },
+        });
       }
     } else if (request) {
       return {
