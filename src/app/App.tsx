c591caac2147a31e00578da072534456c0828eb0
Jerry Ejonavi <jerryasih@gmail.com>
2020-05-30 16:26:01 +0100

    remove App.css

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,5 @@
 import React, { FC, lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import './App.css';
 import { useSelector } from 'react-redux';
 import { RootState } from '../rootReducer';
 
